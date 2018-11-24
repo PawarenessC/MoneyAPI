72c00d63acaf4fe86930cf22795d162414488f01
yuko fuyutsuki <yukofuyutsuki@gmail.com>
2018-11-24 15:49:21 +0900

    APIの更新、コード整理

--- src/MoneyAPI/Main.php
+++ src/MoneyAPI/Main.php
@@ -17,55 +17,63 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 
-class Main extends pluginBase implements Listener{
+class Main extends pluginBase implements Listener {
 
+	/** @var Config */
+	private $money;
 
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+ 		$this->money = new Config($this->getDataFolder() ."money.yml", Config::YAML);//Configを作る
+ 		$this->getLogger()->info("MoneyAPIを読み込みました。");
+	}
 
+	public function onJoin(PlayerJoinEvent $event){
+		$player = $event->getPlayer();
+		$name = $player->getName();
+		if(!$this->money->exists($name, true)){//Configに小文字大文字区別せず名前が存在しなかったら
+      		$this->money->set($name, 0);// Configにセットする、初期金額は0
+			$this->money->save();// 反映を忘れずに
+			$player->sendMessage("{$name} さんの経済データを生成しました。");//これいる?笑
+    	}
+	}
 
-public function onEnable() {
- $this->getServer()->getPluginManager()->registerEvents($this, $this);
- $this->Server = $this->getServer();
- $this->money = new Config($this->getDataFolder() ."money.yml", Config::YAML);//Configを作る
- $this->getLogger()->notice("MoneyAPIを読み込みました。");
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+		if ($sender instanceof Player) {
+			$name = $sender->getName();//名前取得
+			$sender->sendMessage(
+				"§e{$name}さんの§aステータス\n".
+				"§b所持金§f:§6".$this->mymoney($name)."円持ってるぜwwww"
+			);
+		}else {
+			$this->getLogger()->warning("エラー: ゲーム内から実行して下さい");
+		}
+		return true;
+	}
 
+	/**
+	 * 金額の取得
+	 * @param string $name
+	 * @return int
+	 */
+	public function mymoney(string $name): int {
+		if($this->money->exists($name, true)){// Configに大文字小文字区別せずその名前($name)があったら...
+			return $this->money->get($name);// 金額を返す
+		}else{//もしもなかったら...
+			$this->money->set($name,"0");//Configにセット
+			$this->money->save();
+			return 0;// 0とする
+		}
+	}
+
+	/**
+	 * 金額の追加
+	 * @param string $name
+	 * @param int $add
+	 */
+	public function addmoney(string $name, int $add) {
+		$money = $this->mymoney($name);// 現在の金額の取得
+		$this->money->set($name, $money + $add);// 増やす
+		$this->money->save();// ファイルに反映
+	}
 }
-
-public function onJoin(PlayerJoinEvent $event){
- $player = $event->getPlayer();
- $name = $player->getName();
- 
-     if(!$this->money->exists($name)){//Configに名前が存在しなかったら
-      $this->money->set($name, "0");//Configにセットする、初期金額は0円
-      $this->money->save();//セーブを忘れずに
-      $player->sendMessage("{$name}さんの経済データを生成しました。");//これいる?笑
-    }
-}
-
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) :bool{
-if(count($args) === 0);
- switch ($command->getName()){
-  
-  case "money";//コマンド名
-  	$name = $sender->getName();//名前取得
-  	$sender->sendMessage("§e{$name}さんの§aステータス");
-	$sender->sendMessage("§b所持金§f:§6".$this->mymoney($name)."円持ってるぜwwww");
-     return true;
-     break;
- }
-}
-
-   public function mymoney($name){//金額取得
-     if($this->money->exists($name)){//Configにその名前($name)があったら...
-     return $this->money->get($name);//金額を返す
-     }else{//もしもなかったら...
-         $this->money->set($name,"0");//Configにセット
-         $this->money->save();
-          return 0;//0円だったよー
-   }
-  }
-   public function addmoney($name,$money){//金額を増やす
-    $this->money->set($name,$this->money->get($name) + $money);//取得して増やす
-    $this->money->save();//セーブじゃ
-     }
-      
- }
